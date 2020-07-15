505e35b0bfaef0f1bcd082fad1049cb7d9fa497c
li2he1 <peterli2he1@gmail.com>
2020-07-14 22:28:34 -0400

    Keyword extraction Monkey Learn API

--- src/main/java/external/GitHubClient.java
+++ src/main/java/external/GitHubClient.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
@@ -69,6 +70,24 @@ public class GitHubClient {
 	}
 	private List<Item> getItemList(JSONArray array) {
 		List<Item> itemList = new ArrayList<>();
+List<String> descriptionList = new ArrayList<>();
+		
+		for (int i = 0; i < array.length(); i++) {
+			// We need to extract keywords from description since GitHub API
+			// doesn't return keywords.
+			String description = getStringFieldOrEmpty(array.getJSONObject(i), "description");
+			if (description.equals("") || description.equals("\n")) {
+				descriptionList.add(getStringFieldOrEmpty(array.getJSONObject(i), "title"));
+			} else {
+				descriptionList.add(description);
+			}	
+		}
+
+		// We need to get keywords from multiple text in one request since
+		// MonkeyLearnAPI has limitations on request per minute.
+		List<List<String>> keywords = MonkeyLearnClient
+				.extractKeywords(descriptionList.toArray(new String[descriptionList.size()]));
+
 		for (int i = 0; i < array.length(); ++i) {
 			JSONObject object = array.getJSONObject(i);
 			Item item = Item.builder()
@@ -77,7 +96,9 @@ public class GitHubClient {
 					.address(getStringFieldOrEmpty(object, "location"))
 					.url(getStringFieldOrEmpty(object, "url"))
 					.imageUrl(getStringFieldOrEmpty(object, "company_logo"))
-					.build();	
+					.keywords(new HashSet<String>(keywords.get(i)))
+					.build();
+	
 			itemList.add(item);
 		}
 		
